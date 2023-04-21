5292182ff636f776d32a9da7ed9b75b120e2396e
Johannekh <johanne.haakenstad@icloud.com>
2023-04-21 07:52:25 +0000

    endret

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Johannekh/sem03simpleserver
+
+go 1.18
+
+require github.com/Johannekh/is105sem03 v0.0.0-20230421071600-659acc7d196a
--- tcp_server.go
+++ tcp_server.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log"
 	"net"
+"github.com/Johannekh/is105sem03/mycrypt"
+
 	"sync"
 )
 
@@ -36,7 +38,9 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switch msg := string(dekryptertMelding); msg {
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
@@ -56,8 +60,4 @@ func main() {
 	wg.Wait()
 
 
-
-dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-log.Println("Dekrypter melding: ", string(dekryptertMelding))
-switch msg := string(dekrypterMelding) { ...
 }
