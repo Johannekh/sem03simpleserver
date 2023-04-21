c7d8e76b350527509acb44c6ffe652168bf88484
Johannekh <johanne.haakenstad@icloud.com>
2023-04-21 07:31:05 +0000

    endret

--- tcp_server.go
+++ tcp_server.go
@@ -52,5 +52,12 @@ func main() {
 			}(conn)
 		}
 	}()
+
 	wg.Wait()
+
+
+
+dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+log.Println("Dekrypter melding: ", string(dekryptertMelding))
+switch msg := string(dekrypterMelding) { ...
 }
